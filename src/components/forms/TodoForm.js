2811e9dad17380581e8e164210abe9b5eca91898
iAriel <ariel@aluno.fapce.edu.br>
2021-11-18 22:30:24 -0300

    category done

--- src/components/forms/TodoForm.js
+++ src/components/forms/TodoForm.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect, useRef} from 'react'
 
 function TodoForm(props) {
     const [input, setInput] = useState(props.edit ? props.edit.value: '');
+    const [radioValue, setRadioValue] = useState(props.edit ? props.edit.value: '')
 
     const inputRef = useRef(null);
 
@@ -13,18 +14,25 @@ function TodoForm(props) {
         setInput(e.target.value);
     };
 
+    const handleChangeRadio = e =>{
+        setRadioValue(e.target.value);
+    }
+
     const handleSubmit = e => {
         e.preventDefault();
-    
+        
         props.onSubmit({
           id: Math.floor(Math.random() * 10000),
-          text: input
+          text: input,
+          category: radioValue
         });
         setInput('');
       };
 
     return (
+        
         <form className="todo-form" onSubmit={handleSubmit}> 
+            <h1>O que faremos hoje ?</h1>
             {props.edit ? (
                 <>
                     <input 
@@ -36,6 +44,16 @@ function TodoForm(props) {
                         onChange={handleChange}
                         ref={inputRef}
                     />
+                    <div className="todo-radio">
+                        <label for="personal">Pessoal
+                            <input type="radio" id="personal" name="category" value="pessoal" checked={true}/>
+                        </label> 
+                        
+                        <label for="work">Trabalho
+                            <input type="radio" id="work" name="category" value="trabalho" />
+                        </label>
+                        
+                    </div>
                     <button className="todo-button"> Atualizar </button>
                 </>
             ) : (
@@ -49,7 +67,14 @@ function TodoForm(props) {
                     onChange={handleChange}
                     ref={inputRef}
                 />
+                <div className="todo-radio">
+                    <label for="personal">Pessoal</label> 
+                    <input type="radio" id="personal" name="category" value="pessoal" onChange={handleChangeRadio}/>
+                    <label for="work">Trabalho</label>
+                    <input type="radio" id="work" name="category" value="trabalho" onChange={handleChangeRadio}/>
+                </div>
                 <button className="todo-button"> Adicionar </button>
+                
                 </>
             )
             }
--- src/components/list/TodoList.js
+++ src/components/list/TodoList.js
@@ -4,6 +4,7 @@ import Todo from '../todo/Todo'
 
 function TodoList() {
     const [todos, setTodos] = useState([]);
+    const [workTodos, setWorkTodos] = useState([]);
 
     
 
@@ -12,44 +13,106 @@ function TodoList() {
             return
         }
 
-        const newTodos = [todo, ...todos];
-
-        setTodos(newTodos)
+        if(todo.category === "pessoal"){
+            const newTodos = [todo, ...todos];
+            setTodos(newTodos)
+        }else if(todo.category === "trabalho"){
+            const newTodos = [todo, ...workTodos];
+            setWorkTodos(newTodos)
+        }
+        console.log(todos, workTodos)
+        
     };
 
     const removeTodo = id =>{
-        const remoreArr = [...todos].filter(todo => todo.id !== id);
-        setTodos(remoreArr)
+        const existId = todos.find(todo => {
+            if(todo.id === id){
+                return todo;
+            }
+        });
+
+        if(existId){
+            const remoreArr = [...todos].filter(todo => todo.id !== id);
+            setTodos(remoreArr)
+        }else{
+            const remoreArrWokr = [...workTodos].filter(todo => todo.id !== id);
+            setWorkTodos(remoreArrWokr)
+        }
+
+        
     };
 
     const updatedTodo = (todoId, newValue) => {
         if(!newValue.text || /^\s*$/.test(newValue.text)){
             return
         }
+        const existId = todos.find(todo => {
+            if(todo.id === todoId){
+                return todo;
+            }
+        });
 
-        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+        if(existId){
+            setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+        }else{
+            setWorkTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+        }
+        
     };
 
     const completeTodo = id => {
-        let updatedTodo = todos.map(todo => {
-            if(todo.id === id){
-                todo.isComplete = !todo.isComplete
+        const existId = todos.find(todo => {
+            if(todo.id == id){
+                return todo;
             }
-            return todo
-        })
-        setTodos(updatedTodo)
+        });
+
+        if(existId) {
+            let updatedTodo = todos.map(todo => {
+                if(todo.id === id){
+                    todo.isComplete = !todo.isComplete
+                }
+                return todo
+            })
+            setTodos(updatedTodo)
+        }else{
+            let updatedWorkTodo = workTodos.map(todo => {
+                if(todo.id === id){
+                    todo.isComplete = !todo.isComplete
+                }
+                return todo
+            })
+            setWorkTodos(updatedWorkTodo)
+        }
+        
+        
     };
 
     return (      
         <div>
-            <h1>O que temos que fazer hoje ?</h1>
             <TodoForm onSubmit={addTodo} />
-            <Todo 
-                todos={todos} 
-                completeTodo={completeTodo} 
-                removeTodo={removeTodo}
-                updateTodo={updatedTodo}
-            />
+            <div  className="todo-column">
+                <div className="personal-todos">
+                    <label> Pessoal </label>
+                    <Todo
+                        todos={todos} 
+                        completeTodo={completeTodo} 
+                        removeTodo={removeTodo}
+                        updateTodo={updatedTodo}
+                    />
+                </div>
+                <div className="work-todos">
+                    <label> Trabalho </label>
+                    <Todo 
+                        todos={workTodos} 
+                        completeTodo={completeTodo} 
+                        removeTodo={removeTodo}
+                        updateTodo={updatedTodo}
+                    />
+                </div>
+                
+            </div>
+            
         </div>
     )
 }
